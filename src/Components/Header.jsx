8e966b7b066045295e5c0832d0e5fce8b9254809
sparshbarolia <107509768+sparshbarolia@users.noreply.github.com>
2023-12-21 14:13:03 +0530

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
                         </a>
 
                         <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                            <li><a href="#" className="nav-link px-2 text-secondary">Home</a></li>
+                            <li><a href="#" className="nav-link px-2 text-white">Home</a></li>
                             <li><a href="#" className="nav-link px-2 text-white">Features</a></li>
                             <li><a href="#" className="nav-link px-2 text-white">Pricing</a></li>
                             <li><a href="#" className="nav-link px-2 text-white">FAQs</a></li>
